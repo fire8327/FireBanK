4953d78006a4d000d62c77c49ac9bdbacca46d13
fire8327 <114702513+fire8327@users.noreply.github.com>
2024-02-29 18:22:10 +0300

    second project commit

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+    <!-- <Header/> -->
+    <main class="grid-container gap-y-[50px] md:gap-y-20 xl:gap-y-[105px] grow">
+        <slot/>
+    </main>
+    <!-- <Footer/> -->
+</template>
